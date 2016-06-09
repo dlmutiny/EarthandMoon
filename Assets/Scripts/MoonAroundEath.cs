15851352681b2816f742c69e059c219bc28f431c
dlmutiny <lemberger.david@gmail.com>
2016-06-09 10:02:47 -0700

    Earth and Moon Animated Hologram
    Simple animated hologram for Hololens. Made in Unity to be compiled and
    imported to VS. Load scene "MainHololens.Unity"

---
+++ Assets/Scripts/MoonAroundEath.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoonAroundEath : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        transform.RotateAround(Vector3.zero, Vector3.up, -1 * Time.deltaTime);
+
+    }
+}
